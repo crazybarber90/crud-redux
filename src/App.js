a4086aacf565691d97a294496309c05858519cac
crazybarber90 <pepy90aa@gmail.com>
2022-07-28 12:17:19 +0200

    added redux env , created fakeData of users, and added functionality for adding new users

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
 import "./App.css";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addUser } from "./features/Users";
 
 function App() {
+  const userList = useSelector((state) => state.users.value);
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+
   return (
     <div className="App">
-      <h1>Checking pushing on git </h1>
+      <div className="addUser">
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            dispatch(
+              addUser({
+                id: userList[userList.length - 1].id + 1,
+                name,
+                username,
+              })
+            );
+          }}
+        >
+          {" "}
+          Add User{" "}
+        </button>
+      </div>
+      <div className="displayUsers">
+        {userList.map((user) => {
+          return (
+            <div>
+              <h1>{user.name}</h1>
+              <h1>{user.username}</h1>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/FakeData.js
@@ -0,0 +1,42 @@
+export const UsersData = [
+  {
+    id: 1,
+    name: "Nikola Petrovic",
+    username: "Pepi",
+  },
+  {
+    id: 2,
+    name: "Nenad Petrovic",
+    username: "Nesa",
+  },
+  {
+    id: 3,
+    name: "Zorica Petrovic",
+    username: "Zoka",
+  },
+  {
+    id: 4,
+    name: "Nebojsa Petrovic",
+    username: "Sone",
+  },
+  {
+    id: 5,
+    name: "Ivica Petrovic",
+    username: "Iva",
+  },
+  {
+    id: 6,
+    name: "Izabela Petrovic",
+    username: "Ana",
+  },
+  {
+    id: 7,
+    name: "Lazar Petrovic",
+    username: "Laza",
+  },
+  {
+    id: 8,
+    name: "Milan Petrovic",
+    username: "Mica",
+  },
+];
